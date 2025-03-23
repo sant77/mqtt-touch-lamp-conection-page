df52db60bf9c7d11dd392843839b97311a12f3d4
Santiago Duque Ramos <san7790.sg@gmail.com>
2025-03-22 22:36:22 -0500

    [refactor] dashboard

--- frontend/src/app/components/dashboard/device/device.component.ts
+++ frontend/src/app/components/dashboard/device/device.component.ts
@@ -1,12 +1,141 @@
-import { Component } from '@angular/core';
+// dashboard.component.ts
+import { Component, ViewChild } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTable, MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { CommonModule } from '@angular/common';
+import { MatDialog } from '@angular/material/dialog';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faPlugCircleCheck, faPlugCircleExclamation } from '@fortawesome/free-solid-svg-icons';
+import { Router } from '@angular/router';
+import { DashboardService } from '../dashboard-service';
+import { MqttService } from '../mqtt-service';
+import { ModalAddDeviceComponent } from '../modal-add-device/modal-add-device.component';
+
+
+export interface UserDeviceRelation {
+  dispositivo: string;
+  nombre: string;
+  description: string;
+  id: string;
+}
 
 @Component({
   selector: 'app-device',
   standalone: true,
-  imports: [],
+  imports: [
+          MatSidenavModule, 
+          MatToolbarModule, 
+          MatButtonModule, 
+          MatTableModule, 
+          MatIconModule, 
+          CommonModule, 
+          FontAwesomeModule],
   templateUrl: './device.component.html',
   styleUrl: './device.component.css'
 })
 export class DeviceComponent {
+  showFiller = false;
+  device_relation_user: UserDeviceRelation[] = [];
+  dataSourceDevice = this.device_relation_user;
+  columnsToDisplayDevice = ['dispositivo', 'nombre'];
+  columnsToDisplayWithExpandDevice = [...this.columnsToDisplayDevice, 'expand'];
+  expandedElementDevice!: UserDeviceRelation | null;
 
+  @ViewChild(MatTable)
+  table!: MatTable<UserDeviceRelation>;
+
+  constructor(private dialog: MatDialog, 
+              private dashboardService: DashboardService, 
+              private router: Router, 
+              private mqttService:MqttService,
+              ) {}
+
+  ngOnInit(): void {
+    this.loadData();
+  }
+
+  loadData(): void {
+    this.getDeviceUserRelations();
+  }
+
+  getDeviceUserRelations(): void {
+    this.dashboardService.getDeviceUserRelations().subscribe(
+      (data) => {
+        this.device_relation_user = data;
+        this.dataSourceDevice = this.device_relation_user;
+        this.table.renderRows();
+      },
+      (error) => {
+        console.error('Error al obtener los tipos de dispositivos', error);
+      }
+    );
+  }
+
+  deleteDeviceRow(row: UserDeviceRelation): void {
+    const index = this.dataSourceDevice.indexOf(row);
+    if (index > -1) {
+      this.dashboardService.deleteDeviceRelation(row.id).subscribe(
+        () => {
+          this.dataSourceDevice.splice(index, 1);
+          this.table.renderRows();
+        },
+        (error) => {
+          console.error('Error al eliminar el dispositivo', error);
+        }
+      );
+    }
+  }
+
+
+  toggleRowDevice(row: UserDeviceRelation): void {
+    this.expandedElementDevice = this.expandedElementDevice === row ? null : row;
+  }
+
+  openModalDevice(): void {
+    const dialogRef = this.dialog.open(ModalAddDeviceComponent, {
+      width: '500px',
+      data: {}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.getDeviceUserRelations();
+      }
+    });
+  }
+
+  quick_session(): void {
+    localStorage.clear();
+    this.router.navigate(['/']);
+  }
+
+  sendMqttMessageOn(row:UserDeviceRelation): void{
+    const index = this.dataSourceDevice.indexOf(row);
+    console.log(this.dataSourceDevice[index]);
+    this.mqttService.sendMqttMessage(this.dataSourceDevice[index]["id"], "On").subscribe(
+      () => {
+        console.log("Mensaje enviado.")
+      },
+      (error) => {
+        console.error("Error al enviar mensaje.", error);
+      }
+    );
+  }
+
+  sendMqttMessageOff(row:UserDeviceRelation): void{
+    const index = this.dataSourceDevice.indexOf(row);
+    console.log(this.dataSourceDevice[index]);
+    this.mqttService.sendMqttMessage(this.dataSourceDevice[index]["id"], "Off").subscribe(
+      () => {
+        console.log("Mensaje enviado.")
+      },
+      (error) => {
+        console.error("Error al enviar mensaje.", error);
+      }
+    );
+}
 }
--- frontend/src/app/components/dashboard/user-conection/user-conection.component.ts
+++ frontend/src/app/components/dashboard/user-conection/user-conection.component.ts
@@ -1,25 +1,124 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+// dashboard.component.ts
+import { Component, ViewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTable, MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import {MatCardModule} from '@angular/material/card';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { CommonModule } from '@angular/common';
+import { MatDialog } from '@angular/material/dialog';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { Router } from '@angular/router';
+import { faPlugCircleCheck, faPlugCircleExclamation } from '@fortawesome/free-solid-svg-icons';
+import { DashboardService } from '../dashboard-service';
+import { ModalAddConectionComponent } from '../modal-add-conection/modal-add-conection.component';
+
+
+export interface UserConection {
+  Conexion: string;
+  Tipo: string;
+  Estatus: boolean;
+  publish: string;
+  subcribe: string;
+  id: string;
+}
 
 @Component({
   selector: 'app-user-conection',
-  standalone: true,
-  imports: [
-    CommonModule,
-    MatCardModule,
-    MatInputModule,
-    MatButtonModule,
-    MatIconModule,
-    MatTooltipModule
-  ],
+   standalone: true,
+    imports: [MatSidenavModule, 
+              MatToolbarModule, 
+              MatButtonModule, 
+              MatTableModule, 
+              MatIconModule, 
+              CommonModule, 
+              FontAwesomeModule,
+              UserConectionComponent],
+    animations: [
+      trigger('detailExpand', [
+        state('collapsed,void', style({ height: '0px', minHeight: '0' })),
+        state('expanded', style({ height: '*' })),
+        transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+      ]),
+    ],
   templateUrl: './user-conection.component.html',
   styleUrl: './user-conection.component.css'
 })
 export class UserConectionComponent {
- 
+
+    faPlugCircleExclamation = faPlugCircleExclamation;
+    faPlugCircleCheck = faPlugCircleCheck;
+    relation_user: UserConection[] = [];
+    showFiller = false;
+    dataSource = this.relation_user;
+    columnsToDisplay = ['conexion', 'tipo', 'estatus'];
+    columnsToDisplayWithExpand = [...this.columnsToDisplay, 'expand'];
+    expandedElement!: UserConection | null;
+
+
+  
+    @ViewChild(MatTable)
+    table!: MatTable<UserConection>;
+  
+    constructor(private dashboardService: DashboardService,
+                private dialog: MatDialog, 
+                private router: Router
+                ) {}
+  
+    ngOnInit(): void {
+      this.loadData();
+    }
+  
+    loadData(): void {
+      this.getUserRelations();
+    }
+
+    getUserRelations(): void {
+      this.dashboardService.getUserRelations().subscribe(
+        (data) => {
+          this.relation_user = data;
+          this.dataSource = this.relation_user;
+          this.table.renderRows();
+        },
+        (error) => {
+          console.error('Error al obtener los tipos de dispositivos', error);
+        }
+      );
+    }
+  
+    toggleRow(row: UserConection): void {
+      this.expandedElement = this.expandedElement === row ? null : row;
+    }
+  
+  
+    openModalConection(): void {
+      const dialogRef = this.dialog.open(ModalAddConectionComponent, {
+        width: '500px',
+        data: {}
+      });
+  
+      dialogRef.afterClosed().subscribe(result => {
+        if (result) {
+          this.getUserRelations();
+        }
+      });
+    }
+
+  
+    deleteConnectionRow(row: UserConection): void {
+      const index = this.dataSource.indexOf(row);
+      if (index > -1) {
+        this.dashboardService.deleteUserRelation(row.id).subscribe(
+          () => {
+            this.dataSource.splice(index, 1);
+            this.table.renderRows();
+          },
+          (error) => {
+            console.error('Error al eliminar la conexión', error);
+          }
+        );
+      }
+    }
+  
 }
